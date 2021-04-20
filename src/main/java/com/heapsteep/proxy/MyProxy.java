69cdeb848cee6b09485c698f1f8de2df3bf87945
Prasanna Sahu <prasannasahu4u@gmail.com>
2021-04-20 20:02:46 +0530

    commit

--- src/main/java/com/heapsteep/proxy/MyProxy.java
+++ src/main/java/com/heapsteep/proxy/MyProxy.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.heapsteep.bean.Microservice2;
 
-@FeignClient(name="microservice-1", url="localhost:8000")
+//@FeignClient(name="microservice-1", url="localhost:8000")
+@FeignClient(name="microservice-1")
 public interface MyProxy {
 	@GetMapping("microservice-1/from/{from}/to/{to}")
 	public Microservice2 getValue(@PathVariable String from, @PathVariable String to);
