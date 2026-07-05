fbb97fe33aa2b282234c2518aac4c4e93fdcf131
prasanna-sahu <prasanna.sahu@mindnerves>
2026-07-05 11:13:01 +0530

    distributed tracing changes

--- src/main/java/com/heapsteep/controller/GreetingClientController.java
+++ src/main/java/com/heapsteep/controller/GreetingClientController.java
@@ -5,6 +5,8 @@ import com.heapsteep.client.httpinterface.Microservice1HttpInterfaceClient;
 import com.heapsteep.client.resttemplate.Microservice1RestTemplateClient;
 import com.heapsteep.client.webclient.Microservice1WebClient;
 import com.heapsteep.model.GreetingResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/clients")
 public class GreetingClientController {
+
+	private static final Logger log = LoggerFactory.getLogger(GreetingClientController.class);
 
 	private final Microservice1RestTemplateClient restTemplateClient;
 	private final Microservice1WebClient webClient;
@@ -30,21 +34,25 @@ public class GreetingClientController {
 
 	@GetMapping("/rest-template/{name}")
 	public GreetingResponse getGreetingViaRestTemplate(@PathVariable String name) {
+		log.info("Calling microservice-1 via RestTemplate for name={}", name);
 		return restTemplateClient.getGreeting(name);
 	}
 
 	@GetMapping("/webclient/{name}")
 	public GreetingResponse getGreetingViaWebClient(@PathVariable String name) {
+		log.info("Calling microservice-1 via WebClient for name={}", name);
 		return webClient.getGreeting(name);
 	}
 
 	@GetMapping("/feign/{name}")
 	public GreetingResponse getGreetingViaFeign(@PathVariable String name) {
+		log.info("Calling microservice-1 via Feign for name={}", name);
 		return feignClient.getGreeting(name);
 	}
 
 	@GetMapping("/http-interface/{name}")
 	public GreetingResponse getGreetingViaHttpInterface(@PathVariable String name) {
+		log.info("Calling microservice-1 via HTTP Interface for name={}", name);
 		return httpInterfaceClient.getGreeting(name);
 	}
 }
